8fc83d9dfba7f30abe9d15eff11448c82568c267
Luch3nz0 <enzo_bc@hotmail.com>
2024-02-21 12:52:47 -0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-
-// Example: Toggle comments
-const commentsButton = document.querySelector('.comments-button');
-const commentsSection = document.querySelector('.comments-section');
-
-commentsButton.addEventListener('click', () => {
-    commentsSection.classList.toggle('hidden');
-});
